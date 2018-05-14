9ab4b1aa557280db3db4c1d4229e85c3d4d7e132
ZachCurtis <31259055+ZachCurtis@users.noreply.github.com>
2018-05-14 19:09:37 -0400

    fix bad commit
    had filenames wrong sorry

--- src/Server/Factory.lua
+++ src/Server/Factory.lua
@@ -1,259 +1,155 @@
-local v3 =  Vector3.new
-local cf = CFrame.new
-local cfa = CFrame.Angles
-local rad = math.rad
-local noMass = PhysicalProperties.new(.01,1,1)
+local signal = require(script.Signal)
+local rigWorker = require(script.RigBuilder)
 
-local part = Instance.new('Part')
-part.TopSurface = Enum.SurfaceType.Smooth
-part.BottomSurface = Enum.SurfaceType.Smooth
-part.Anchored = true
-part.Size = v3(1,1,1)
+local httpSer = game:GetService('HttpService')
 
-local a = Instance.new('Attachment')
-a.Parent = part
-a.Name = 'Hub'
+local eventList = {
+	ChassisCreated = true,
+	ChassisBuilding = true
+}
 
-local weld = Instance.new('Weld')
+local mt = {
+	__newindex = function(t,k,v)
+		if eventList[k] == true then
+			rawset(t,k,signal.new())
+		else
+			rawset(t,k,v)
+		end
+	end
+}
 
-local ball = part:Clone()
-ball.Shape = 'Ball'
-ball.Name ='WheelCollider'
-
-part.CanCollide = false
-
-local vecf = Instance.new('VectorForce')
-vecf.Force = v3(0,0,0)
-
-local RigBuilder = {}
-
-function RigBuilder.new(settings, modelRef, owner )
+local Factory = {}
 	
-	local self = {}
+function Factory.NewChassis(buildSettings, modelRef, owner)
+
+	local rigBuilder = rigWorker.new(buildSettings, modelRef, owner)
 	
 	local main = modelRef.PrimaryPart
 	
-	local function RemoveMass(thing)
-		for _, v in pairs(thing:GetDescendants()) do
-			if v:IsA('BasePart') then
-				v.CustomPhysicalProperties = noMass
-			end
-		end
-	end
+	local wheelWeld = modelRef:FindFirstChild('WheelWelded')
+	local parts = modelRef:FindFirstChild('Parts')
 	
-	local function DontCollide()
-		
-	end
+	local chassisId = httpSer:GenerateGUID(false)
 	
-	local function SetNetwork(modelRef, owner)
-		for _, v in pairs(modelRef:GetDescendants()) do
-			if (v:IsA('BasePart') and v.ClassName ~= 'Terrain') then
-				if v.Anchored == false then
-					v:SetNetworkOwner(owner)
-				end
-			end
-		end
-	end
-	
-	local function SingleWeld(p0, p1)
-		local w = weld:Clone()
-		w.Parent = p0
-		w.Part0 = p0
-		w.Part1 = p1
-		w.C0 = p0.CFrame:Inverse() * p1.CFrame
-	end
-	
-	local function WeldWheel(wheel, visualsModel)
-		RemoveMass(visualsModel)
+	-- fire prebuild event if it exists
+	if Factory.ChassisBuilding then
+		if (type(Factory.ChassisBuilding) == 'boolean') and Factory.ChassisBuilding ~= true then
 		
-		local visModel = Instance.new('Model')
-		visModel.Parent = wheel.Parent
-		
-		if visualsModel:FindFirstChild('Main') then
-			if visualsModel.Main:FindFirstChild('ColliderCenter') then
-				local wOrient = wheel.Hub.WorldOrientation
-				if string.lower(string.sub(wheel.Parent.Name, 2,2)) == 'r' then
-					wOrient = v3(wOrient.x, wOrient.y, -wOrient.z)
-				end
-				visualsModel:SetPrimaryPartCFrame((wheel.CFrame * cfa(rad(wOrient.X), rad(wOrient.Y), rad(wOrient.Z))) * cf(visualsModel.Main.ColliderCenter.Position))
-			end
-		end
-		
-		for _, v in pairs(visualsModel:GetDescendants()) do 
-			if v:IsA('BasePart') then
-				local w = SingleWeld(wheel, v)
-			end
-		end
-	end
-	
-	
-	local function UnanchorAll(model)
-		for _, v in pairs(model:GetDescendants()) do
-			
-			if v:IsA('BasePart') then
-				if v.ClassName ~= 'Terrain' then
-					v.Anchored = false
-				end
-			end
-		end
-	end
-	
-	local function GetAxles()
-		local axleF = main.CFrame * cf((main.CFrame.lookVector.Unit * settings.FrontAxleOffset + main.CFrame.upVector.Unit * settings.FrontAxleHeight))
-		local axleR = axleF * cf(-axleF.lookVector.Unit * settings.Wheelbase)
-		
-		return axleF, axleR
-	end
-	
-	-- todo: size ball by wheelsizes
-	local function GetWheels(axleF, axleR)
-		local fl, fr, rl, rr = ball:Clone(), ball:Clone(), ball:Clone(), ball:Clone()
-		
-		local wheelsL, wheelsR = {fl, rl, }, {fr, rr}
-		
-		for _, v in pairs(wheelsL) do
-			v.Size = v3(settings.WheelColliderRadius, settings.WheelColliderRadius, settings.WheelColliderRadius)
-			v.CFrame = axleF *cf(-axleF.rightVector.Unit * (settings.FrontAxleWidth / 2))
-			v.Hub.Orientation = v3(0,0,-90)
-			
-			if settings._isDev == true then
-				v.Transparency = .6
-			else
-				v.Transparency = 1
-			end
-		end
-		
-		for _, v in pairs(wheelsR) do
-			v.Size = v3(settings.WheelColliderRadius, settings.WheelColliderRadius, settings.WheelColliderRadius)
-			v.CFrame = axleF *cf(axleF.rightVector.Unit * (settings.FrontAxleWidth / 2))
-			v.Hub.Orientation = v3(0,0,90)
-			
-			if settings._isDev == true then
-				v.Transparency = .6
-			else
-				v.Transparency = 1
-			end
-		end
-		
-		return fl, fr, rl, rr
-	end
-	
-	local function GetSuspension(wheel)
-		
-		local sideVar = 'Rear'
-		if string.lower(string.sub(wheel.Name,1,1)) == 'f' then
-			sideVar = 'Front'
-		end
-		
-		local mount = part:Clone()
-		mount.Name = 'StrutMount'
-		mount.Hub.Orientation = v3(0,0,-90)
-		mount.CFrame = wheel.CFrame * cf(wheel.CFrame.upVector.Unit * settings[sideVar .. 'SuspensionHeight'])
-		
-		if settings._isDev == true then
-			mount.Transparency = .6
 		else
-			mount.Transparency = 1
+			Factory.ChassisBuilding:Fire(chassisId)
 		end
-	
-		return mount
 	end
 	
-	local function RigSpring(wheel, mount)
+	rigBuilder.RemoveMass(modelRef)
+	rigBuilder.DontCollide(modelRef)
+	
+	-- calc axleF from main offset then axleR from 
+	local axleF, axleR = rigBuilder.GetAxles()
+	
+	local fl, fr, rl, rr = rigBuilder.GetWheels(axleF, axleR)
+	local wheels = {
+		['fl'] = fl,
+		['fr'] = fr,
+		['rl'] = rl,
+		['rr'] = rr
+	}
+	
+	local wheelModels = {}
+	
+	local suspension = {}
+	local constraints = {}
+	local wheelForces = {}
+	--local suspensionForces = {}
+
+	if not modelRef:FindFirstChild('Wheels') then
+		local m = Instance.new("Model")
+		m.Parent = modelRef
+		m.Name ='Wheels'
+	end
+	
+	for k,v in pairs(wheels) do
+		local keyedWheel = modelRef.Wheels:FindFirstChild(k)
+		--local keyedWeld = wheelWeld:Clone()
 		
-		local sideVar = 'Rear'
-		if string.lower(string.sub(wheel.Name,1,1)) == 'f' then
-			sideVar = 'Front'
+		if not keyedWheel then
+			keyedWheel = Instance.new("Model")
+			keyedWheel.Name = k
+			keyedWheel.Parent = modelRef.Wheels
 		end
 		
-		local spring = Instance.new('SpringConstraint')
+		v.Parent = keyedWheel
+
+		suspension[k] = rigBuilder.GetSuspension(v)
+		suspension[k].Parent = keyedWheel
 		
-		spring.Parent = mount
-		spring.Attachment0 = mount.Hub
-		spring.Attachment1 = wheel.Hub
-		spring.Visible = settings._isDev and true or false
-		spring.FreeLength = settings[sideVar .. 'SuspensionHeight']
-		spring.Stiffness = settings[sideVar .. 'Stiffness']
-		spring.Damping = 	settings[sideVar .. 'Damping']	
+		constraints[k] = {
+			Spring = rigBuilder.RigSpring(v,suspension[k]),
+			Dampener = rigBuilder.RigDampener(v,suspension[k])
+		}
+	
+		wheelForces[k] = rigBuilder.RigWheelTorque(v)
 		
-		return spring
+		wheelModels[k] = keyedWheel
+		
+		v.Parent = keyedWheel
 	end
 	
-	local function RigDampener(wheel, mount)
-		
-		local sideVar = 'Rear'
-		if string.lower(string.sub(wheel.Name,1,1)) == 'f' then
-			sideVar = 'Front'
+	for k, v in pairs(suspension) do
+		local w = rigBuilder.SingleWeld(main, v)
+	end
+	
+	if wheelWeld then
+		for k, v in pairs(wheelModels) do
+			local visuals = wheelWeld:Clone()
+			visuals.Parent = v
+			rigBuilder.WeldWheel(v.WheelCollider, visuals)
+			wait()
+			rigBuilder.UnanchorAll(v)
 		end
 		
-		local damp = Instance.new('CylindricalConstraint')
-		damp.Parent = mount
-		damp.Attachment0 = mount.Hub
-		damp.Attachment1 = wheel.Hub
-		
-		damp.InclinationAngle = 90
-		damp.LimitsEnabled = true
-		damp.LowerLimit = settings[sideVar .. 'SuspensionHeight'] / 2
-		damp.UpperLimit = settings[sideVar .. 'SuspensionHeight']
-		damp.Visible = settings._isDev and true or false
-		
-		return damp
+		wheelWeld:Destroy()
 	end
 	
-	local function RigSpringCompresser(mount)
-		local comp = vecf:Clone()
-		
-		return comp
+	wait(.1)
+	
+	local massF, massR, massC = rigBuilder.RigMass(axleF, axleR)
+	
+	local aeroF, aeroR, aeroC = rigBuilder.RigAero(axleF, axleR)
+	
+	if parts then
+		rigBuilder.RemoveMass(parts)
+		for _, v in pairs(parts:GetDescendants()) do
+			if (v:IsA('BasePart') and v.ClassName ~= 'Terrain') then
+				v.CanCollide = false
+				rigBuilder.SingleWeld(main, v)
+			end
+		end
 	end
 	
-	local function RigWheelTorque(wheel)
-		local tq = Instance.new('Torque')
-		tq.Parent = wheel
-		tq.Attachment0 = wheel.Hub
-		
-		return tq
+	rigBuilder.UnanchorAll(modelRef)
+	
+	if owner then
+		rigBuilder.SetNetwork(modelRef, owner)
+	
 	end
 	
-	local function RigMass()
-		local massF, massR, massC = part:Clone(), part:Clone(), part:Clone()
-
-		local massf = settings.GrossWeight * (settings.WeightDistribution  / 100)
-		local massr = settings.GrossWeight - massf
-		
-		
-		return massF, massR, massC
+	if buildSettings._isDev == true then
+	
+	else
+	
 	end
 	
-	local function RigAero(axleF, axleR)
-		local aeroF, aeroR, aeroC = vecf:Clone(), vecf:Clone(), vecf:Clone()
-		aeroF.Name, aeroR.Name, aeroC.Name = 'aeroF', 'aeroR', 'aeroC'
+	if Factory.ChassisCreated then
+		if (type(Factory.ChassisCreated) == 'bool' or 'boolean') and Factory.ChassisCreated ~= true then
 		
-		local hub = a:Clone()
-		
-		hub.Parent = main
-		
-		aeroC.Parent = main
-		aeroC.Attachment0 = hub
-		return aeroF, aeroR, aeroC
+		else
+			Factory.ChassisCreated:Fire(chassisId)
+		end
 	end
-
-	self.RemoveMass = RemoveMass
-	self.UnanchorAll = UnanchorAll
-	self.DontCollide = DontCollide
-	self.SetNetwork = SetNetwork
-	self.WeldWheel = WeldWheel
-	self.SingleWeld = SingleWeld
-	self.GetAxles = GetAxles
-	self.GetWheels = GetWheels
-	self.GetSuspension = GetSuspension
-	self.RigSpring = RigSpring
-	self.RigDampener = RigDampener
-	self.RigSpringCompresser = RigSpringCompresser
-	self.RigWheelTorque = RigWheelTorque
-	self.RigMass = RigMass
-	self.RigAero = RigAero
-
-	return self
+	
+	return true
 end
 
-return RigBuilder
+setmetatable(Factory, mt)
+
+return Factory
