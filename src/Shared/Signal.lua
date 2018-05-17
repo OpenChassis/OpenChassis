87f2673ab4559c64fce77e2b29ebe0fa1117597f
ZachCurtis <31259055+ZachCurtis@users.noreply.github.com>
2018-05-17 09:55:27 -0400

    Create Signal.lua

---
+++ src/Shared/Signal.lua
@@ -0,0 +1,59 @@
+-- closure scoping reimplement of https://github.com/LPGhatguy/lemur/blob/master/lib/Signal.lua
+local Signal = {}
+Signal.__index = Signal
+
+local function listInsert(list, ...)
+	local args = {...}
+	local newList = {}
+	local listLen = #list
+		
+	for i = 1, listLen do
+		newList[i] = list[i]
+	end
+		
+	for i = 1, #args do
+		newList[listLen + i] = args[i]
+	end
+		
+	return newList
+end
+	
+local function listValueRemove(list, value)
+	local newList = {}
+
+	for i = 1, #list do
+		if list[i] ~= value then
+			table.insert(newList, list[i])
+		end
+	end
+		
+	return newList
+end
+
+function Signal.new()
+	local self = setmetatable({}, Signal)
+	
+	local boundCallbacks = {}
+	
+	function self:Connect(cb)
+
+		boundCallbacks = listInsert(boundCallbacks, cb)
+
+		local function disconnect()
+			boundCallbacks = listValueRemove(boundCallbacks, cb)
+		end
+		
+		return {Disconnect = disconnect}
+	end
+	
+	function self:Fire(...)
+		
+		for i = 1, #boundCallbacks do
+			boundCallbacks[i](...)
+		end
+	end
+	
+	return self
+end
+
+return Signal
