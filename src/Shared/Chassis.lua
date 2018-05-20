adde76fcd7eec2304f190a8c88d847bd6d16f96d
ZachCurtis <31259055+ZachCurtis@users.noreply.github.com>
2018-05-20 19:13:52 -0400

    mover framework

--- src/Shared/Chassis.lua
+++ src/Shared/Chassis.lua
@@ -2,11 +2,17 @@ local httpSer = game:GetService("HttpService")
 local runSer = game:GetService('RunService')
 local dataBuilder = require(game.ReplicatedStorage:WaitForChild('Shared').DataBuilder)
 local signal = require(game.ReplicatedStorage:WaitForChild('Shared').Signal)
+local helper = require(game.ReplicatedStorage:WaitForChild('Shared').ChassisHelper)
+
 local chassisEvent = game.ReplicatedStorage:WaitForChild('Events').OCEvent
 
 local cf = CFrame.new
 local v3 = Vector3.new
 local v2 = Vector2.new 
+
+local pi = math.pi
+
+local ran = Random.new(tick())
 
 local Chassis = {}
 Chassis.__index = Chassis
@@ -24,27 +30,62 @@ function Chassis.new(id, settings, modelref, owner)
 	local newChassis = {}
 	setmetatable(newChassis, Chassis)
 	
-	local _guid = id  
+	local _guid		= id  
 	local _settings = settings
-	local _owner = owner
-	local _driver = false
+	local _owner	= owner
+	local _driver   = false
 	
-	local _master = false
+	local _master  = false
 	local _starter = false
+	local _running = false
+	
+	local _lightsOn  = false
+	local _isLowBeam = false
 	
 	local _rpm = 0
-	local _throttle = 0
+
+	local _gear = 2
 	
-	local _gear = 1
+	local _allWheels, _driveWheels = dataBuilder:GetWheelColliders(modelref, _settings.DriveType)
 	
-	local _allTorque, _driveTorque, _aeroForce = dataBuilder:GetForces(modelref, settings.DriveType)
-	local _steer = dataBuilder:GetSteering(modelref, settings.SteerType)
+	local _allTorque, _driveTorque, _aeroForce = dataBuilder:GetForces(modelref, _settings.DriveType)
+	
+	local _steer = dataBuilder:GetSteering(modelref, _settings.SteerType)
 	
 	local _exhaust, _starter, _idle, _lockBeep, _tireSounds = dataBuilder:GetSounds(modelref)
 	
-	local _rearRunning, _brake, _low, _high, _fog, _turnLeft, _turnRight = dataBuilder:GetLights(modelref)
+	local _rearRunning, _brake, _reverse, _low, _high, _fog, _turnLeft, _turnRight = dataBuilder:GetLights(modelref)
 	
 	newChassis.Model = modelref
+	
+	-- scoped helper funcs
+	local function lightToggle(lightList, state)
+		for i = 1 ,#lightList do
+			local v = lightList[i]
+			v.Material = (state and Enum.Material.Neon or Enum.Material.SmoothPlastic)
+			
+			local hasLight = v:FindFirstChildOfClass('SurfaceLight') or v:FindFirstChildOfClass('PointLight') or v:FindFirstChildOfClass('SpotLight')
+			
+			if hasLight then
+				hasLight.Enabled = state
+			end
+		end
+	end
+	
+	local function applyTorque(tQ)
+		if #_driveWheels == 4 then
+			-- is awd
+			-- first split between axles then wheels
+		else
+			-- is fwd or rwd
+			-- just split between these wheels
+		end
+	end
+	
+	-- inputs
+	newChassis.Throttle = 0
+	newChassis.Clutch   = 0
+	newChassis.Brakes   = 0
 	
 	newChassis.Locked = false
 	
@@ -53,6 +94,8 @@ function Chassis.new(id, settings, modelref, owner)
 	newChassis.DriverChanged = signal.new()
 	newChassis.DriverEntered = signal.new()
 	newChassis.DriverExited  = signal.new()
+	newChassis.EngineStarted = signal.new()
+	newChassis.EngineStopped = signal.new()
 	
 	newChassis.GetId = function()
 		return _guid
@@ -89,6 +132,10 @@ function Chassis.new(id, settings, modelref, owner)
 	end
 	
 	newChassis.StopDriving = function(playerRequesting)
+		if runSer:IsClient() then
+			chassisEvent:FireServer('StopDriving', newChassis.GetId())
+		end
+		
 		if _driver == playerRequesting then
 			_driver = false
 			
@@ -103,23 +150,147 @@ function Chassis.new(id, settings, modelref, owner)
 	
 	newChassis.ToggleMaster = function()
 		_master = not _master
+		
+		if _master == false then
+			if _running then
+				_idle:Stop()
+				_exhaust:Stop()
+				_starter:Stop()
+				_running = false
+				
+				newChassis.EngineStopped:Fire()
+			end
+		end
 	end
 	
 	newChassis.ToggleStarter = function ()
 		_starter = not _starter
+		
+		if _starter == true then
+			if not _running then
+				spawn(function()
+					_starter:Play()
+					
+					wait(ran:NextNumber(.5, 3))
+					
+					_idle:Play()
+					_running = true
+					
+					newChassis.EngineStarted:Fire()
+				end)
+			else
+				-- over crank
+			end
+		else
+			_starter:Stop()
+		end
 	end
 	
-	newChassis.PowerWheels =  function()
-		
+	newChassis.GearUp = function()
+		if _gear ~= #_settings.GearRatios then
+			if _gear == 1 then
+				lightToggle(_reverse, false)
+				_gear = 2
+			else
+				_gear = _gear + 1
+			end
+		end
 	end
 	
-	newChassis.Steer = function()
+	newChassis.GearDown = function()
+		if _gear == 2 then
+			lightToggle(_reverse, true)
+			_gear = 1
+		else
+			_gear = _gear - 1
+		end
+	end
+	
+	newChassis.ToggleLights = function()
+		_lightsOn = not _lightsOn
 		
+		if not _master then
+			lightToggle(_high, false)
+			lightToggle(_low, false)
+			lightToggle(_rearRunning, false)
+			return false
+		end
+		
+		if _lightsOn == false then
+			lightToggle(_high, false)
+			lightToggle(_low, false)
+			lightToggle(_rearRunning, false)
+		else
+			lightToggle(_rearRunning, true)
+			if _isLowBeam then
+				lightToggle(_low, true)
+			else
+				lightToggle(_high, true)
+			end
+		end
+	end
+	
+	newChassis.ToggleLamps = function()
+		if not _master then
+			return false
+		end
+		
+		if _lightsOn then
+			_isLowBeam = not _isLowBeam
+			
+			if _isLowBeam == true then
+				lightToggle(_low, false)
+				lightToggle(_high, true)
+			end
+		end
 	end
 	
 	newChassis.LockBeep = function()
 		if _lockBeep then
 			_lockBeep:Play()
+		end
+	end
+	
+	-- call in renstep on client, heartbeat on server
+	newChassis.Update = function()
+		
+		if not _running then
+			return false
+		end
+		
+		if newChassis.Clutch == 0 then
+			local topDriveVelc = helper:GetFastestRotation(_driveWheels)
+			
+			if topDriveVelc == 0 then
+				-- _rpm = _settings.Idle
+				-- thats for autos ^
+				-- lets stall mannys ayy?
+			else
+				_rpm = (topDriveVelc * _settings.GearRatios[_gear] * _settings.FinalDrive) * (60/2 * pi)
+			end
+			
+			local maxTorque = helper:PowerCurveLookup(_rpm, _settings.Curve)
+			local flyWheelTorque = (newChassis.Throttle <= .05 and maxTorque * .05 or maxTorque * newChassis.Throttle)
+			
+			local driveLineTorque = flyWheelTorque * _settings.GearRatios[_gear] * _settings.FinalDrive * .7 -- mechanical loss factor of 30%
+			
+			-- driveLineTorque is NOT scaled for roblox!!!
+			-- add brake torque to it then pass to wheels
+		end
+	end
+	
+	newChassis.Destroy = function()
+		
+		newChassis.DriverChanged:Disconnect()
+		newChassis.DriverEntered:Disconnect()
+		newChassis.DriverExited:Disconnect()
+		newChassis.EngineStarted:Disconnect()
+		newChassis.EngineStopped:Disconnect()
+	
+		if runSer:IsClient() then
+			chassisEvent:FireServer('Destroy', newChassis.GetId())
+		else
+			modelref:Destroy()
 		end
 	end
 	
@@ -139,8 +310,6 @@ function Chassis:Unlock(playerRequesting)
 end
 
 function Chassis:Lock(playerRequesting)
-	print(runSer:IsClient())
-	print(runSer:IsServer())
 	if runSer:IsClient() then
 		chassisEvent:FireServer('Lock', self.GetId())
 		return 
@@ -151,10 +320,6 @@ function Chassis:Lock(playerRequesting)
 	else
 		return false
 	end
-end
-
-function Chassis:Update()
-	
 end
 
 return Chassis
